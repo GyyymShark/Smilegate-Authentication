bcfd9e010a57dedf090bcb82beed606d653227d4
the-groot <sangwoo0795@naver.com>
2022-12-16 21:38:37 +0900

    start project

---
+++ authentication/authentication/src/test/java/smilgate/winterdev/authentication/AuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package smilgate.winterdev.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
