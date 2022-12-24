221c1211acb6251a8e3c33da7f4d846d4feca4dd
the-groot <sangwoo0795@naver.com>
2022-12-24 15:52:32 +0900

    유저정보 json으로 호출

--- frontend/router/mypage/mypage.js
+++ frontend/router/mypage/mypage.js
@@ -19,65 +19,9 @@ connection.connect();       //mysql 연동
 
 router.get('/',function(req,res){   //임시로 마이페이지 조회
     //판매목록, 구매목록, 약속목록 데이터 전달해줌
-    var userId=req.user;
-    if(userId){
-        if(userId === 999){
-            res.redirect('/admin');
-        }
-        else{
-            var sql1=`SELECT productName, price, photoLink,productId,
-            sellerId, statusId
-            FROM mypageproduct_information
-            WHERE sellerId=${userId} AND statusId=2;`; //내가 판매한 제품의 이름과 가격을 전달해주는 쿼리
     
-            var sql2=`SELECT productName, price, photoLink, productId,
-            buyerId, statusId
-            FROM mypageproduct_information 
-            WHERE buyerId=${userId} AND statusId=2;`;    //내가 구매한 제품의 이름과 가격을 전달해주는 쿼리
-    
-            var sql3=`SELECT productId, productName, price, photoLink
-            FROM mypageproduct_information
-            WHERE (buyerId=${userId} OR sellerId=${userId}) AND statusId=1;`   //약속 목록 정보 조회 쿼리
-    
-            var sql4=`SELECT productName, price, photoLink, productId, nickname
-            FROM detailproduct_information
-            WHERE userId=${userId};`        //작성글 목록 정보
-    
-            console.log("마이페이지 유저 : " + req.user);
-            if(req.user){           //현재 로그인한 유저가 있으면 
-                var sql5 = "select nickname from user where id=" + req.user + ";";
-    
-                connection.query(sql1+sql2+sql3+sql4+sql5, function(err, data){
-                if(err) throw err;
-        
-                var sql_data1=data[0];  //판매 목록 정보
-                var sql_data2=data[1];  //구매 목록 정보
-                var sql_data3=data[2];  //약속 목록 정보
-                var sql_data4=data[3];  //작성글 목록 정보
-                var sql_data5=data[4][0].nickname;  //작성글 목록 정보
-                
-    
-                res.render('mypage', {sell: sql_data1, buy: sql_data2, promise:sql_data3, product:sql_data4, user:sql_data5});
-        
-                })
-            }
-            else{           //현재 로그인한 유저 없으면
-                connection.query(sql1+sql2+sql3+sql4, function(err, data){
-                if(err) throw err;
-        
-                var sql_data1=data[0];  //판매 목록 정보
-                var sql_data2=data[1];  //구매 목록 정보
-                var sql_data3=data[2];  //약속 목록 정보
-                var sql_data4=data[3];  //작성글 목록 정보
-    
-                res.render('mypage', {sell: sql_data1, buy: sql_data2, promise:sql_data3, product:sql_data4});
-                })
-            }
-        }
-    }
-    else{
-        res.render('mypage');
-    }
+ res.render('mypage');
+
 })
 
 
